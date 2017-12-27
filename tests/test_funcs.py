ad63baa9f9db66d252ebcec7c84a70543599a91d
Laurent Courty <lrntct@gmail.com>
2017-12-27 12:10:11 -0600

    add tests for topo export

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -2,6 +2,7 @@
 """
 
 import pyswashes
+import numpy as np
 
 
 def test_swashes_dataframe():
@@ -22,3 +23,23 @@ def test_15d_dataframe():
 def test_2d_dataframe():
     s = pyswashes.TwoDimensional(1, 1, 1, 10, 10)
     s.dataframe()
+
+
+def test_1d_dem():
+    s = pyswashes.OneDimensional(1, 1, 2, 10)
+    arr = s.topo()
+    assert isinstance(arr, np.ndarray)
+    assert arr.ndim == 1
+
+
+def test_15d_dataframe():
+    s = pyswashes.PseudoTwoDimensional(1, 1, 1, 10)
+    arr = s.topo()
+    assert isinstance(arr, np.ndarray)
+
+
+def test_2d_dataframe():
+    s = pyswashes.TwoDimensional(1, 1, 1, 10, 10)
+    arr = s.topo()
+    assert isinstance(arr, np.ndarray)
+    assert arr.ndim == 2
