a30eb8e2ad30d5826eaf6bc10254ff99264652bb
Laurent Courty <lrntct@gmail.com>
2018-01-04 18:15:39 -0600

    cosmetic changes in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import io
 from setuptools import setup, find_packages
 
 
 # see setup.cfg
 metadata = dict(packages=find_packages('src'),
                 package_dir={'': 'src'}
-                )
+               )
 
 
 setup(**metadata)
